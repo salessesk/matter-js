80085a41805f97d1b9f690669cdf5f61612c8025
salessesk <salesses.kevin@gmail.com>
2017-09-13 11:15:45 +0200

    Update matter.js

--- build/matter.js
+++ build/matter.js
@@ -1746,7 +1746,7 @@ var Body = _dereq_('./Body');
      * @returns {bounds} The composite bounds.
      */
     Composite.bounds = function(composite) {
-        var bodies = Matter.Composite.allBodies(composite),
+        var bodies = Composite.allBodies(composite),
             vertices = [];
 
         for (var i = 0; i < bodies.length; i += 1) {
@@ -1754,7 +1754,7 @@ var Body = _dereq_('./Body');
             vertices.push(body.bounds.min, body.bounds.max);
         }
 
-        return Matter.Bounds.create(vertices);
+        return Bounds.create(vertices);
     };
 
     /*
